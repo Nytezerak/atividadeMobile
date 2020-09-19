87bfd8c231fa0df0547a452a5cf17a72186e4c84
Nytezerak <chrysdragon.97@hotmail.com>
2020-09-19 10:22:32 +0900

    Atualização geral

--- www/components/plugins.js
+++ www/components/plugins.js
@@ -1,2 +1,22 @@
-// This is a JavaScript file
+$(document).on("click","#alerta",function(){
+  navigator.notification.alert("Hello, World! :D", null, "=u=", ":P");
+});
 
+$(document).on("click","#confirm",function(){
+  function confirma(buttonIndex){
+    if(buttonIndex==1){
+      navigator.notification.alert("Selecionou A");
+    }else{
+      (navigator.notification.alert("Selecionou B"));
+    }
+  }
+  navigator.notification.confirm("Selecione A ou B", confirma, "Escolha: ", ["A","B"]);
+});
+
+$(document).on("click","#beep",function(){
+      navigator.notification.beep(3);
+});
+
+$(document).on("click","#vibrar",function(){
+      navigator.vibrate(4000);
+});
